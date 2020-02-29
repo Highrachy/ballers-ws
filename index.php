d5ff4d0960ceff940f9adb04bfab53be4f455962
Samuel Sola-Eniafe <tobie@Tobies-MacBook-Pro.local>
2020-02-29 14:28:15 +0100

    ft: fix mail sending [Finishes #171431816, #171431848]

--- index.php
+++ index.php
@@ -10,6 +10,7 @@ if((isset($_POST['fullname'])&& $_POST['fullname'] !='') && (isset($_POST['email
     $phone = $mysqli->real_escape_string($_POST['phone']);
 
     $sqlQuery = "INSERT INTO `visitors`(`fullName`, `email`, `phone`) VALUES ('$fullName','$email','$phone')";
+    $message = "Fullname: $fullName \nEmail: $email \nPhone: $phone";
 
     if(!$mysqli->query($sqlQuery)){
         die("An error occured ".$mysqli->error);
@@ -67,6 +68,9 @@ if((isset($_POST['fullname'])&& $_POST['fullname'] !='') && (isset($_POST['email
           </div>
         </div>
     </div>
+
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
   </body>
 </html>
 

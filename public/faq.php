ea6e0999e63ab4574e22f48f1c51166eab653f37
Samuel Sola-Eniafe <tobie@Tobies-MacBook-Pro.local>
2020-04-17 02:35:01 +0100

    ft: faq desktop view

--- public/faq.php
+++ public/faq.php
@@ -1,6 +1,166 @@
 <?php require_once('includes/header.php'); ?>
 
-<section class="holding">
-    <h1>FAQ here</h1>
+<section class="faq-holding">
+    <h1 class="faq-holding-heading">Frequently Asked Questions</h1>
+    <h6 class="faq-holding-text">Are you looking for an answer to your questions about BALL? <br> Here we have compiled an overview of frequently asked questions we receive from our BALLers</h6>
+
+    <section class="faq-holding-form col-lg-6 col-md-11 col-12">
+        <form class="form-inline">
+            <label class="sr-only" for="search">Search</label>
+            <div class="input-group col-lg-8 col-12">
+              <div class="input-group-prepend">
+                <div class="input-group-text"><img src="./assets/img/icons/search-icon.png" class="input-icon" alt="location icon"></div>
+              </div>
+              <input type="text" class="form-control hold-input" id="search" placeholder="Search for a question...">
+            </div>
+          
+            <div class="input-group col-lg-4 col-12">
+                <button type="submit" class="btn btn-primary btn-large faq-holding-search-btn">Search</button>
+            </div>
+        </form>
+    </section>
 </section>
+
+<section class="faq-category">
+    <h6 class="faq-category-text">or chooose a category to quickly find the help you need</h6>
+
+    <div class="faq-category-slider">
+        <div class="faq-category-slide">
+            <img src="./assets/img/icons/profile-icon.png" class="faq-category-slide-img" alt="Profile">
+            <h5 class="faq-category-slide-text">Profile</h5>
+        </div>
+        <div class="faq-category-slide">
+            <img src="./assets/img/icons/geting-started-icon.png" class="faq-category-slide-img" alt="Profile">
+            <h5 class="faq-category-slide-text">Getting Started</h5>
+        </div>
+        <div class="faq-category-slide">
+            <img src="./assets/img/icons/payment-icon.png" class="faq-category-slide-img" alt="Profile">
+            <h5 class="faq-category-slide-text">Payment</h5>
+        </div>
+        <div class="faq-category-slide">
+            <img src="./assets/img/icons/security-icon.png" class="faq-category-slide-img" alt="Profile">
+            <h5 class="faq-category-slide-text">Security</h5>
+        </div>
+    </div>
+</section>
+
+<section class="faq-getting-started">
+    <h2 class="faq-getting-started-heading">Getting Started</h2>
+    <h5 class="faq-getting-started-text">This are questions on the general topic on about BALL. Please go <br> through this section if you need your questions answered.</h5>
+
+    <div class="col-lg-9 col-12 faq-getting-started-accordion">
+        <div id="accordion">
+            <div class="card">
+              <div class="card-header" id="headingOne">
+                <a class="" data-toggle="collapse" href="#collapseOne" aria-expanded="false" aria-controls="collapseOne">
+                  <h5 class="mb-0 faq-card-heading">
+                    What is BALL?
+                    <span class="icon-toggle">
+                      <span class="faq-getting-started-icon one">-</span>
+                    </span>
+                  </h5>
+                </a>
+              </div>
+              <div id="collapseOne" class="collapse show" aria-labelledby="headingOne" data-parent="#accordion">
+                <div class="card-body">
+                  Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod.
+                </div>
+              </div>
+            </div>
+  
+            <div class="card">
+              <div class="card-header" id="headingTwo">
+                <a class="" data-toggle="collapse" href="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+                  <h5 class="mb-0 faq-card-heading">
+                    What is special about BALL?
+                    <span class="icon-toggle">
+                      <span class="faq-getting-started-icon">+</span>
+                    </span>
+                  </h5>
+                </a>
+              </div>
+              <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordion">
+                <div class="card-body">
+                  Balling is an experience, a choice for a quality lifestyle and a legacy, generations to come will benefit from an easy decision you make today.
+                </div>
+              </div>
+            </div>
+  
+            <div class="card">
+              <div class="card-header" id="headingThree">
+                <a class="" data-toggle="collapse" href="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+                  <h5 class="mb-0 faq-card-heading">
+                    What is the minimum amount to invest?
+                    <span class="icon-toggle">
+                      <span class="faq-getting-started-icon">+</span>
+                    </span>
+                  </h5>
+                </a>
+              </div>
+              <div id="collapseThree" class="collapse" aria-labelledby="headingThree" data-parent="#accordion">
+                <div class="card-body">
+                  Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod.
+                </div>
+              </div>
+            </div>
+  
+            <div class="card">
+              <div class="card-header" id="headingFour">
+                <a class="" data-toggle="collapse" href="#collapseFour" aria-expanded="false" aria-controls="collapseFour">
+                  <h5 class="mb-0 faq-card-heading">
+                    How secured are my savings?
+                    <span class="icon-toggle">
+                      <span class="faq-getting-started-icon">+</span>
+                    </span>
+                  </h5>
+                </a>
+              </div>
+              <div id="collapseFour" class="collapse" aria-labelledby="headingFour" data-parent="#accordion">
+                <div class="card-body">
+                  Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod.
+                </div>
+              </div>
+            </div>
+  
+            <div class="card">
+              <div class="card-header" id="headingFive">
+                <a class="" data-toggle="collapse" href="#collapseFive" aria-expanded="false" aria-controls="collapseFive">
+                  <h5 class="mb-0 faq-card-heading">
+                    Can I upgrade my saving plan in the future?
+                    <span class="icon-toggle">
+                      <span class="faq-getting-started-icon">+</span>
+                    </span>
+                  </h5>
+                </a>
+              </div>
+              <div id="collapseFive" class="collapse" aria-labelledby="headingFive" data-parent="#accordion">
+                <div class="card-body">
+                  Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod.
+                </div>
+              </div>
+            </div>
+  
+          </div>
+    </div>
+</section>
+
+<section class="faq-question">
+    <h2 class="faq-question-heading">You still have a question?</h2>
+    <h5 class="faq-question-text">If you cannot find answer to your question in our FAQ, you can <br> always contact us . We will be with you shortly!</h5>
+
+    <div class="faq-question-contact col-12">
+        <div class="col-lg-4 col-md-5 col-12 faq-question-info-div">
+            <img src="./assets/img/icons/phone-icon.png" alt="Phone icon">
+            <h4 class="faq-question-info">+234 807 654 5543</h4>
+            <p class="faq-question-info-small">We are always happy to help.</p>
+        </div>
+        &nbsp;&nbsp;&nbsp;
+        <div class="col-lg-4 col-md-5 col-12 faq-question-info-div">
+            <img src="./assets/img/icons/mail-icon.png" alt="Mail icon">
+            <h4 class="faq-question-info">support@ballers.ng</h4>
+            <p class="faq-question-info-small">The best way to get answer faster.</p>
+        </div>
+    </div>
+</section>
+
 <?php require_once('includes/footer.php'); ?>

e00ae187af1f23f04c4f0984a35f5f10d874b8ff
Samuel Sola-Eniafe <tobie@Tobies-MacBook-Pro.local>
2020-05-03 01:45:30 +0100

    ft: a-z pages

---
+++ public/az.php
@@ -0,0 +1,245 @@
+<?php require_once('includes/header.php'); ?>
+
+<section class="az-holding">
+    <h1 class="az-holding-heading">A-Z of BALL</h1>
+    <h5 class="az-holding-text">Great minds BALL, while they are BALLing, they are called BALLers.. <br>Are you ready to Become A LandLord? Come BALL with Us. In life, it’s <br> not enough to just exist you have to BALL.</h5>
+</section>
+
+<section class="az-ball row">
+    <div class="col-lg-5 col-12">
+        <h2 class="az-ball-heading">What is BALL?</h2>
+    </div>
+    <div class="col-lg-7 col-12">
+        <h6 class="az-ball-title">BALL EXPLAINED</h6>
+        <p class="az-ball-text">BALL is a platform that enables her members to seamlessly own quality homes in well planned communities in a sustainable way. <br>We guide you through the entire process which is customized specially for you.</p>
+    </div>
+    <div class="col-lg-5 col-12"></div>
+    <div class="col-lg-7 col-12">
+        <h6 class="az-ball-title">ORIGIN OF BALL</h6>
+        <p class="az-ball-text">Balling is an experience, a choice for a quality lifestyle and a legacy, generations to come will benefit from an easy decision you make today.</p>
+    </div>
+</section>
+
+<section class="az-easy">
+    <h2 class="az-easy-heading">BALLing is as easy as ABC</h2>
+    
+    <div class="az-easy-abc col-lg-3 col-sm-5 col-11">
+        <img class="az-easy-abc-letter" src="./assets/img/icons/a.png" alt="A">
+        <h5 class="az-easy-abc-title"><br> Apply now </h5>
+        <p class="az-easy-abc-text">Take control of the future and create a timeless legacy by filling the application form below</p>
+    </div>
+
+    <div class="az-easy-abc col-lg-3 col-sm-5 col-11">
+        <img class="az-easy-abc-letter" src="./assets/img/icons/b.png" alt="B">
+        <h5 class="az-easy-abc-title">Begin periodic <br> contribution</h5>
+        <p class="az-easy-abc-text">Cultivate the habit of making convenient contributions here and reap the rewards as a BALLer for life</p>
+    </div>
+
+    <div class="az-easy-abc col-lg-3 col-sm-5 col-11">
+        <img class="az-easy-abc-letter" src="./assets/img/icons/c.png" alt="C">
+        <h5 class="az-easy-abc-title">Convert to home <br> ownership</h5>
+        <p class="az-easy-abc-text">Get the home of your dreams using an array of financial products including single digit mortgages as low as 6%</p>
+    </div>
+</section>
+
+<section class="az-how">
+    <h6 class="az-how-title">HOW IT WORKS</h6>
+    <h4 class="az-how-title-2">In three easy steps</h4>
+    <h6 class="az-how-title">STEP A</h6>
+    <h3 class="az-how-heading">APPLICATION</h3>
+
+    <div class="row">
+        <div class="az-how-step col-lg-5 col-sm-5 col-12">
+            <img class="az-how-step-img" src="./assets/img/preference.png" alt="Define preference">
+        </div>
+        <div class="az-how-step-line col-lg-2 col-sm-2 col-12"></div>
+        <div class="az-how-step col-lg-5 col-sm-5 col-12">
+            <h5 class="az-how-step-title">Define your prefrence</h5>
+            <p class="az-how-step-text">Ea fugiat Lorem quis cupidatat nulla. Officia <br> voluptate anim anim laboris. Sunt nostrud ullamc</p>
+        </div>
+
+        <div class="az-how-step col-lg-5 col-sm-5 col-12">
+            <img class="az-how-step-icon" src="./assets/img/icons/location-house.png" alt="Preferred location">
+            <h6 class="az-how-step-title-2">Select preferred <br> location</h6>
+            <p class="az-how-step-text-2">Ea fugiat Lorem quis cupidatat <br> nulla. Officia voluptate <br> anim anim laboris. Sunt <br> nostrud ullamc</p>
+        </div>
+        <div class="az-how-step-line col-lg-2 col-sm-2 col-12"></div>
+        <div class="az-how-step col-lg-5 col-sm-5 col-12">
+            <img class="az-how-step-icon" src="./assets/img/icons/house-type.png" alt="Property Type">
+            <h6 class="az-how-step-title-2">Select preferred <br> property type</h6>
+            <p class="az-how-step-text-2">Ea fugiat Lorem quis cupidatat <br> nulla. Officia voluptate <br> anim anim laboris. Sunt <br> nostrud ullamc</p>
+        </div>
+
+        <div class="az-how-step col-lg-5 col-sm-5 col-12">
+            <img class="az-how-step-img" src="./assets/img/subscription.png" alt="Subscription">
+        </div>
+        <div class="az-how-step-line col-lg-2 col-sm-2 col-12"></div>
+        <div class="az-how-step col-lg-5 col-sm-5 col-12">
+            <h5 class="az-how-step-title">Structure your subscription</h5>
+            <p class="az-how-step-text">Ea fugiat Lorem quis cupidatat nulla. Officia <br> voluptate anim anim laboris. Sunt nostrud ullamc</p>
+        </div>
+
+        <div class="az-how-step col-lg-4 col-sm-6 col-12">
+            <img class="az-how-step-icon" src="./assets/img/icons/payment-amount.png" alt="Payment amount">
+            <h6 class="az-how-step-title-2">Input convienient <br> payment amount</h6>
+            <p class="az-how-step-text-2">Ea fugiat Lorem quis cupidatat <br> nulla. Officia voluptate <br> anim anim laboris. Sunt <br> nostrud ullamc</p>
+        </div>
+        <div class="az-how-step col-lg-4 col-sm-6 col-12">
+            <img class="az-how-step-icon" src="./assets/img/icons/interval.png" alt="Interval">
+            <h6 class="az-how-step-title-2">Select preferred periodic <br> time interval</h6>
+            <p class="az-how-step-text-2">Ea fugiat Lorem quis cupidatat <br> nulla. Officia voluptate <br> anim anim laboris. Sunt <br> nostrud ullamc</p>
+        </div>
+        <div class="az-how-step col-lg-4 col-12">
+            <img class="az-how-step-icon" src="./assets/img/icons/duration.png" alt="Duration">
+            <h6 class="az-how-step-title-2">Calculate the total <br> duration</h6>
+            <p class="az-how-step-text-2">Ea fugiat Lorem quis cupidatat <br> nulla. Officia voluptate <br> anim anim laboris. Sunt <br> nostrud ullamc</p>
+        </div>
+    </div>
+
+    <button class="btn btn-lg big-button">APPLY NOW</button>
+
+    <h6 class="az-how-title">STEP B</h6>
+    <h3 class="az-how-heading">BEGIN PERIODIC <br> CONTRIBUTION</h3>
+
+    <div class="row">
+        <div class="az-how-step col-lg-5 col-sm-5 col-12">
+            <img class="az-how-step-img" src="./assets/img/start-contribution.png" alt="Start contribution">
+        </div>
+        <div class="az-how-step-line col-lg-2 col-sm-2 col-12"></div>
+        <div class="az-how-step col-lg-5 col-sm-5 col-12">
+            <h5 class="az-how-step-title">Start contributing</h5>
+            <p class="az-how-step-text">Ea fugiat Lorem quis cupidatat nulla. Officia <br> voluptate anim anim laboris. Sunt nostrud ullamc</p>
+        </div>
+    </div>
+
+    <h6 class="az-how-title">STEP C</h6>
+    <h3 class="az-how-heading">CONVERT TO <br> HOME OWNERSHIP</h3>
+
+    <div class="row">
+        <div class="az-how-step col-lg-5 col-sm-5 col-12">
+            <img class="az-how-step-img" src="./assets/img/convert-subscription.png" alt="Convert subscription">
+        </div>
+        <div class="az-how-step-line col-lg-2 col-sm-2 col-12"></div>
+        <div class="az-how-step col-lg-5 col-sm-5 col-12">
+            <h5 class="az-how-step-title">Convert subscription</h5>
+            <p class="az-how-step-text">Ea fugiat Lorem quis cupidatat nulla. Officia <br> voluptate anim anim laboris. Sunt nostrud ullamc</p>
+        </div>
+        <div class="az-how-step col-lg-12 col-12">
+            <img class="az-how-step-img" src="./assets/img/move-in.png" alt="Move in">
+            <h5 class="az-how-step-title-2">Move into your new home</h5>
+            <p class="az-how-step-text-2">Ea fugiat Lorem quis cupidatat nulla. Officia <br> voluptate anim anim laboris. Sunt nostrud ullamc</p>
+        </div>
+    </div>
+
+</section>
+
+<section class="benefits">
+    <h6 class="az-benefits-title">BENEFITS</h6>
+    <h2 class="benefits-heading">Why Ballers is Special</h2>
+
+    <div class="benefits-slider">
+      <div class="slide">
+        <img src="./assets/img/icons/income.png" class="slide-image" alt="Recurring Income">
+        <h4 class="slide-header">Recurring income</h4>
+        <p class="slide-body">You can create several income generating streams from just becoming a member; referral income (the best in the industry), bonus points, interest on your contribution.</p>
+      </div>
+      
+      <div class="slide">
+        <img src="./assets/img/icons/estates.png" class="slide-image" alt="Existing estates">
+        <h4 class="slide-header">Existing estates</h4>
+        <p class="slide-body">We brought you the I-Factor and then Blissville, you can clearly see that our mantra is value driven quality real estate that enhances your overall living experience.</p>
+      </div>
+
+      <div class="slide">
+        <img src="./assets/img/icons/credible.png" class="slide-image" alt="Credibility">
+        <h4 class="slide-header">Credibility</h4>
+        <p class="slide-body">Powered by a team of seasoned professionals with extensive track record.</p>
+      </div>
+
+      <div class="slide">
+        <img src="./assets/img/icons/investment.png" class="slide-image" alt="Investment Oriented">
+        <h4 class="slide-header">Investment Oriented</h4>
+        <p class="slide-body">For every contribution you <br> make you get immediate bonus <br> points that can easily be <br> redeemed for a wide range of <br> relaxing and sumptuous social <br> activities.</p>
+      </div>
+
+      <div class="slide">
+        <img src="./assets/img/icons/fluid.png" class="slide-image" alt="Fluidity and Flexibility">
+        <h4 class="slide-header">Fluidity and Flexibility</h4>
+        <p class="slide-body">You can choose any amount to <br> contribute once you have <br> started and you can pause or <br> exit the scheme if so desired.</p>
+      </div>
+    </div>
+</section> 
+
+<section class="az-control">
+    <h2 class="az-control-heading">You have full control</h2>
+
+    <div class="row">
+        <div class="col-lg-1 col-12"></div>
+        <div class="col-lg-5 col-sm-6 col-12">
+            <img class="az-control-panel-img" src="./assets/img/see-available.png" alt="">
+            <h5 class="az-control-panel-heading">See available properties</h5>
+            <p class="az-control-panel-text">Ea fugiat Lorem quis cupidatat nulla. Officia <br> voluptate anim anim laboris. Sunt nostrud ullamc</p>
+        </div>
+        <div class="col-lg-5 col-sm-6 col-12">
+            <img class="az-control-panel-img" src="./assets/img/monitor.png" alt="">
+            <h5 class="az-control-panel-heading">Monitor your contribution</h5>
+            <p class="az-control-panel-text">Ea fugiat Lorem quis cupidatat nulla. Officia <br> voluptate anim anim laboris. Sunt nostrud ullamc</p>
+        </div>
+        <div class="col-lg-1 col-12"></div>
+        <div class="col-lg-1 col-12"></div>
+        <div class="col-lg-5 col-sm-6 col-12">
+            <img class="az-control-panel-img" src="./assets/img/view-map.png" alt="">
+            <h5 class="az-control-panel-heading">View map</h5>
+            <p class="az-control-panel-text">Ea fugiat Lorem quis cupidatat nulla. Officia <br> voluptate anim anim laboris. Sunt nostrud ullamc</p>
+        </div>
+        <div class="col-lg-5 col-sm-6 col-12">
+            <img class="az-control-panel-img" src="./assets/img/conversation.png" alt="">
+            <h5 class="az-control-panel-heading">Start a conversation</h5>
+            <p class="az-control-panel-text">Ea fugiat Lorem quis cupidatat nulla. Officia <br> voluptate anim anim laboris. Sunt nostrud ullamc</p>
+        </div>
+        <div class="col-lg-1 col-12"></div>
+    </div>
+</section>
+
+<section class="az-creed">
+    <img class="az-creed-q-up" src="./assets/img/icons/quote-up.png" alt="quotation mark">
+    <h6 class="az-creed-title">BALLERS CREED</h6>
+    <h4 class="az-creed-heading">As a BALLer, we share a duty to BALL <br> and live a healthy lifestyle for it’s not <br> just a home but also a lifestyle</h4>
+    <img class="az-creed-q-down" src="./assets/img/icons/quote-down.png" alt="quotation mark">
+    <img class="az-creed-q-up-mobile" src="./assets/img/icons/quote-up.png" alt="quotation mark">
+</section>
+
+<section class="az-message row">
+    <div class="col-lg-6 col-12">
+        <h2 class="az-message-heading">Want to know more? <br> Please. Send a message</h2>
+    </div>
+    <div class="col-lg-6 col-12">
+        <div class="col-10 contact-hello-form">
+            <h4 class="contact-hello-heading">Send a message</h4>
+            <form method="post" action="">
+                <?php require_once('includes/mail.php') ?>
+                <div class="form-group">
+                    <label for="name">Your Name *</label>
+                    <input type="text" class="form-control" id="contact-name" name="name" placeholder="" required>
+                </div>
+                <div class="form-group">
+                    <label for="email">Your Email *</label>
+                    <input type="email" class="form-control" id="contact-email" name="email" placeholder="" required>
+                </div>
+                <div class="form-group">
+                    <label for="message">Your Message *</label>
+                    <input type="text" class="form-control" id="contact-message" name="message" placeholder="" required>
+                </div>
+                <button type="submit" class="btn btn-lg contact-hello-btn">Send Message</button>
+            </form>
+        </div>
+    </div>
+</section>
+
+<section class="about-gallery">
+    <h6 class="about-gallery-title">START BALLING</h6>
+    <h2 class="about-gallery-heading">Take a deep dive into <br> what your home looks like</h2>
+    <button class="btn btn-lg big-button">SIGN UP NOW</button>
+</section>
+
+<?php require_once('includes/footer.php'); ?>

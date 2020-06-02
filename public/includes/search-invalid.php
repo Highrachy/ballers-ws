f87242fd9a66aced04bcd1165507a48586e42268
Samuel Sola-Eniafe <tobie@Tobies-MacBook-Pro.local>
2020-06-02 20:36:12 +0100

    ft: add no search

---
+++ public/includes/search-invalid.php
@@ -0,0 +1,6 @@
+<section class="search-no-result">
+    <img src="./assets/img/no-results.svg" alt="no result found" class="search-no-result-image">
+
+    <h2 class="search-no-result-heading">Sorry. No results found 🙁</h2>
+    <p class="search-no-result-text">We couldn’t find what you searched for. Try searching again.</p>
+</section>
